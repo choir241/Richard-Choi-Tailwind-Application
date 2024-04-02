e2872c960cbc1dc1cb30597eeb199744f66c57fe
Richard Choi <richardchoi54@gmail.com>
2024-04-02 15:16:35 -0400

    add animation on hover for navbar menu, close and links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,25 +9,25 @@ export default function Nav() {
   return (
     <nav className="fixed hidden flex-col self-end bg-black w-full max-[800px]:flex">
       <IoIosMenu
-        className={`text-6xl self-end ${toggleMenu === "hidden" ? "" : "hidden"}`}
+        className={`text-6xl self-end hover:cursor-pointer hover:scale-75 hover:ease-in hover:duration-300 ${toggleMenu === "hidden" ? "" : "hidden"}`}
         onClick={() => setToggleMenu("flex")}
       />
       <IoIosClose
-        className={`text-6xl self-end ${toggleMenu === "hidden" ? "hidden" : ""}`}
+        className={`text-6xl self-end hover:cursor-pointer hover:rotate-90 hover:ease-in hover:duration-300 ${toggleMenu === "hidden" ? "hidden" : ""}`}
         onClick={() => setToggleMenu("hidden")}
       />
       <ul className={`${toggleMenu} items-center flex-col pb-4 px-4`}>
-        <li className="mb-2">
+        <li className="mb-2 hover:translate-x-10 hover:ease-in hover:duration-300">
           <Link className="text-4xl" href="#myProject">
             My Project
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 hover:translate-x-10 hover:ease-in hover:duration-300">
           <Link className="text-4xl" href="#documentation">
             Documentation
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 hover:translate-x-10 hover:ease-in hover:duration-300">
           <Link className="text-4xl" href="#openSource">
             Open Source
           </Link>
